9c1b1183cfd25900f83348756e0746dbe409a75f
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-07-10 14:07:15 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/CarouselRun.java
+++ src/main/java/com/epam/rd/autotasks/CarouselRun.java
@@ -1,13 +1,24 @@
 package com.epam.rd.autotasks;
 
 public class CarouselRun {
+    int position = 0;
+    int[] carouselArray = DecrementingCarousel.carouselArray;
 
     public int next() {
-       throw new UnsupportedOperationException();
+        int count = 0;
+        while (count < carouselArray.length && carouselArray[position %= carouselArray.length] <= 0) {
+            position++;
+            count++;
+        }
+        if (count == carouselArray.length) return -1;
+        return carouselArray[position++]--;
     }
 
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        for (int var: carouselArray) {
+            if (var > 0) return false;
+        }
+        return true;
     }
 
 }
